d75f19e61a99da1a62395c5203d9296d6fc2da30
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 16:42:32 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ function rank() {
 	"author": {
             "@type": "Person",
             "name": "Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025",
-            "reviewBody": "Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!"
+            "reviewBody": "Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online"
           }
         },
     "offers": {
@@ -47,6 +47,22 @@ function rank() {
     };
   }
 
+function histats() {
+    return {
+      __html: `
+        var _Hasync= _Hasync|| [];
+        _Hasync.push(['Histats.start', '1,4921035,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+      `,
+    };
+  };
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -55,7 +71,6 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <meta charSet="utf-8" />
         <title>Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025</title>
         <meta name="description" content="Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online" />
         <meta name="keywords" content="Hoktoto, Slot Bonus New Member 100 Di Awal To 10X, slot Gacor 2025, Deposit Via Dana, Deposit Pulsa Tanpa Potongan, Depo 50 Bonus 50 To 3X, Link Gacor Deposit Dana, Situs Slot Gacor Hari Ini, slot resmi pragmatic play, Link Slot Gates OF Olympus" />
@@ -80,7 +95,7 @@ export default function RootLayout({
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta property="og:title" content="Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025" />
-        <meta property="og:description" content="Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta property="og:description" content="Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online" />
         <meta property="og:url" content={nameDomain} />
         <meta property="og:site_name" content="hoktoto" />
         <meta property="og:locale" content="id_ID" />
@@ -91,12 +106,12 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="hoktoto" />
         <meta name="twitter:title" content="Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025" />
-        <meta name="twitter:description" content="Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta name="twitter:description" content="Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online" />
         <meta name="twitter:image" content={nameDomain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
+        <script dangerouslySetInnerHTML={histats()} type="text/javascript" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
