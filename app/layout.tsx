f3f9a53688e3634bae280c3cf77c8e8f3430aa96
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-20 22:16:52 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { Inter } from 'next/font/google';
+import GlobalConst from "./var-global";
 import './globals.css';
-
-const nameDomain = "https://www.dexterhq.com";
 
 const inter = Inter({
   variable: '--font-inter',
@@ -15,13 +14,13 @@ function rank() {
       __html: `{
     "@context": "https://schema.org",
     "@type": "SoftwareApplication",
-    "name": "https://www.dexterhq.com",
+    "name": '${GlobalConst.domain}',
     "operatingSystem": "ANDROID",
     "applicationCategory": "GameApplication",
     "aggregateRating": {
       "@type": "AggregateRating",
       "ratingValue": "5",
-      "ratingCount": "7143729"
+      "ratingCount": "8142921"
     },
 	"review": {
           "@type": "Review",
@@ -33,8 +32,8 @@ function rank() {
           },
 	"author": {
             "@type": "Person",
-            "name": "Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025",
-            "reviewBody": "Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online"
+            "name": '${GlobalConst.title}',
+            "reviewBody": '${GlobalConst.desc}'
           }
         },
     "offers": {
@@ -51,7 +50,7 @@ function histats() {
     return {
       __html: `
         var _Hasync= _Hasync|| [];
-        _Hasync.push(['Histats.start', '1,4921035,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.start', '${GlobalConst.histats}']);
         _Hasync.push(['Histats.fasi', '1']);
         _Hasync.push(['Histats.track_hits', '']);
         (function() {
@@ -71,16 +70,16 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <title>Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025</title>
-        <meta name="description" content="Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online" />
-        <meta name="keywords" content="Hoktoto, Slot Bonus New Member 100 Di Awal To 10X, slot Gacor 2025, Deposit Via Dana, Deposit Pulsa Tanpa Potongan, Depo 50 Bonus 50 To 3X, Link Gacor Deposit Dana, Situs Slot Gacor Hari Ini, slot resmi pragmatic play, Link Slot Gates OF Olympus" />
-        <meta name="google-site-verification" content="qeql0PmWocmN6Pb_DexNO3owND6oYimXZALrc3-M8KA" />
-        <meta name="application-name" content="hoktoto" />
-        <link rel="author" href={nameDomain} />
-        <link rel="canonical" href={nameDomain} />
-        <meta name="author" content="hoktoto" />
-        <meta name="publisher" content="hoktoto" />
-        <meta name="copyright" content="hoktoto" />
+        <title>{GlobalConst.title}</title>
+        <meta name="description" content={GlobalConst.desc} />
+        <meta name="keywords" content={GlobalConst.keywords} />
+        <meta name="google-site-verification" content={GlobalConst.gsc} />
+        <meta name="application-name" content={GlobalConst.brandName} />
+        <link rel="author" href={GlobalConst.domain} />
+        <link rel="canonical" href={GlobalConst.domain} />
+        <meta name="author" content={GlobalConst.brandName} />
+        <meta name="publisher" content={GlobalConst.brandName} />
+        <meta name="copyright" content={GlobalConst.brandName} />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
         <meta name="category" content="slot" />
@@ -94,20 +93,20 @@ export default function RootLayout({
         <meta name="MobileOptimized" content="width" />
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
-        <meta property="og:title" content="Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025" />
-        <meta property="og:description" content="Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online" />
-        <meta property="og:url" content={nameDomain} />
-        <meta property="og:site_name" content="hoktoto" />
+        <meta property="og:title" content={GlobalConst.title} />
+        <meta property="og:description" content={GlobalConst.desc} />
+        <meta property="og:url" content={GlobalConst.domain} />
+        <meta property="og:site_name" content={GlobalConst.brandName} />
         <meta property="og:locale" content="id_ID" />
-        <meta property="og:image" content={nameDomain} />
+        <meta property="og:image" content={GlobalConst.domain} />
         <meta property="og:image:width" content="840" />
         <meta property="og:image:height" content="480" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="hoktoto" />
-        <meta name="twitter:title" content="Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025" />
-        <meta name="twitter:description" content="Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online" />
-        <meta name="twitter:image" content={nameDomain} />
+        <meta name="twitter:creator" content={GlobalConst.brandName} />
+        <meta name="twitter:title" content={GlobalConst.title} />
+        <meta name="twitter:description" content={GlobalConst.desc} />
+        <meta name="twitter:image" content={GlobalConst.domain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
