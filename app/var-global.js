10e7bf43edbf40f8ec6d9091ab0e1d45699e275b
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-20 21:59:15 +0700

    Create var-global.js

---
+++ app/var-global.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "Hoktoto : Slot Bonus New Member 100 Di Awal To 10X Dan slot Gacor 2025",
+    desc: "Hoktoto hadir dengan promo menarik yang sayang untuk dilewatkan! Nikmati bonus new member 100% di awal hanya dengan melakukan deposit pertama Anda. Dengan turnover (TO) yang rendah, yaitu hanya 10x, Anda bisa lebih mudah meraih kemenangan dan menikmati keuntungan besar. Ini adalah kesempatan sempurna untuk pemain baru yang ingin memulai perjalanan seru di dunia slot online",
+    keywords: "Hoktoto, Slot Bonus New Member 100 Di Awal To 10X, slot Gacor 2025, Deposit Via Dana, Deposit Pulsa Tanpa Potongan, Depo 50 Bonus 50 To 3X, Link Gacor Deposit Dana, Situs Slot Gacor Hari Ini, slot resmi pragmatic play, Link Slot Gates OF Olympus",
+    brandName: "Hoktoto",
+    domain: "https://dexterhq.com",
+    histats: '1,4921035,4,5,172,25,00010000',
+    gsc: 'qeql0PmWocmN6Pb_DexNO3owND6oYimXZALrc3-M8KA',
+    daftar: 'https://daftarwap.orang-dalam.link/hok',
+    login: 'https://daftarwap.orang-dalam.link/hok',
+}
